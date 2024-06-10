a73aba1e10128e0a164f246b0692680d1b279227
Christiaan-Lpt <st10440964@vcconnect.edu.za>
2024-06-10 13:37:14 +0200

    Practicum finished

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     // Declare EditText variables for other days as needed
 
+    //blala
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
